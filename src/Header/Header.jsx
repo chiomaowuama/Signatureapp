5c87df09ad40e6ff74ce08dc6ffffd1cb4180b0c
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-19 15:17:15 +0100

    using tailwind for the glass to ensure responsiveness

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <>
-    <div className='header'>
+    <div className='header border-2 border-green-800 flex justify-between items-center  w-full px-5 py-3 fixed top-0 left-0 before:contents-0 before:absolute before:top-0 before:-left-full'>
       <a href="#" className='logo'>logo</a>
       <div className='navbar'>
         <a href=""> New</a>
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,10 +5,10 @@ function Home() {
     <>
     <div className=' landscape w-full h-screen'>
       <div className='landscapes  w-full h-full flex  justify-center items-center'>
-        <div className='space-y-8 w-4/6  text-4xl text-white capitalize font-bigtext  px-2 '>
+        {/* <div className='space-y-8 w-4/6  text-4xl text-white capitalize font-bigtext  px-2 '>
           <p><span className='text-5xl '>S</span>igns understands that a signature is your unique identity. the opportunity to help you discover what identity best workd securely for you is our goal click here to create your forever siggnature</p>
           <button className='bg-white text-black border-2 border-white rounded-2xl w-2/6 px-2 py-1 shadow-2xl '>Click Here</button>
-        </div>
+        </div> */}
       </div>
         
 
