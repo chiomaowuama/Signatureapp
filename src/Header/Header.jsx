0df8f33beb417c033abf30f386e0304ca767d14d
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-20 12:57:35 +0100

    completed the nav

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -7,31 +7,28 @@ function Header() {
   return (
     <>
     <div className='header  flex  py-5 '>
-      <div className=' w-2/5  flex justify-start items-center px-3 '>
-
-      <Link to="/"  className='logo text-white text-base font-semibold cursor-default  lg:ml-4 '>
-      Signature             
-      </Link>
+      <div className=' w-2/5  flex justify-start items-center px-3 font-bigtext font-semibold'>
+      {/* <Link to="/Home"  className='logo text-white font-bigtext  text-base font-semibold cursor-default  lg:ml-4 '>
+      SIGNATURE             
+      </Link> */}
+      <NavLink to="/"  className='logo text-white font-bigtext  text-base font-semibold cursor-default  lg:ml-4'>
+      SIGNATURE             
+        </NavLink>
       
       </div>
-      <div className='navbar w-3/5 flex justify-around lg:justify-end items-center  lg:gap-4 text-center'>
-        <NavLink to="NewDocument" className={({isActive}) => `text-white ${isActive ? " border-2 border-green-800 text-red-700": "border-2 border-yellow-800 text-blue-700" }`}>
-          New
+      <div className='navbar w-3/5 flex justify-around lg:justify-end items-center   lg:gap-4 text-center font-bigtext font-semibold'>
+        <NavLink to="NewDocument" className={({isActive}) => `text-white lg:w-1/5 px-2 lg:px-0 ${isActive ? "text-red-700": " text-blue-700" }`}>
+          NEW
         </NavLink>
-        <NavLink to="AllDocument" className={({isActive}) => `text-white ${isActive ? " border-2 border-green-800 text-red-700": "border-2 border-yellow-800 text-blue-700" }`}>
-          ALL
+        <NavLink to="AllDocument" className={({isActive}) => `text-white lg:w-1/5 px-2 lg:px-0 ${isActive ? "text-red-700": " text-blue-700" }`}>
+          DOCUMENT
         </NavLink>
-        <NavLink to="SavedDocument" className={({isActive}) => `text-white ${isActive ? " border-2 border-green-800 text-red-700": "border-2 border-yellow-800 text-blue-700" }`}>
-          SAVED
+        <NavLink to="SavedDocument" className={({isActive}) => `text-white lg:w-1/5 px-2 lg:px-0 ${isActive ? "text-red-700": " text-blue-700" }`}>
+          HISTORY
         </NavLink>
         {/* <Navlink to='NewDocument' className={({isActive}) =>`text-white text-base transition-all ${isActive ? "text-red-600" : "text-white"} text-base ml-9`}>
         New
-        </Navlink> */}
-        
-        <a href="NewDocument" className='lg:w-1/5'> New</a>
-        <a href="" className='lg:w-1/5 '>Saved</a>
-        <a href="" className='lg:w-1/5  '>Documents</a>
-        
+        </Navlink> */}        
       </div>
     </div>
     
