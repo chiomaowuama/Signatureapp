a7c9dc5d1e9c712e7b3aea3e107cdacc605a4f77
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-20 12:16:53 +0100

    first push of the day

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
-
-
+// import { Link,Navlink } from 'react-router-dom'
+import { Link} from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 function Header() {
+
   return (
     <>
-    <div className='header border-2 border-green-800 flex justify-between items-center '>
-      <a href="#" className='logo text-white text-base font-semibold cursor-default'>Signature</a>
-      <div className='navbar'>
-        <a href=""> New</a>
-        <a href="">Saved</a>
-        <a href="">Documents</a>
-        {/* <a href=""></a> */}
+    <div className='header  flex  py-5 '>
+      <div className=' w-2/5  flex justify-start items-center px-3 '>
+      {/* <Link to="/">
+      </Link> */}
+      <Link to="/"  className='logo text-white text-base font-semibold cursor-default  lg:ml-4 '>
+      Signature             
+      </Link>
+      
+      </div>
+      <div className='navbar w-3/5 flex justify-around lg:justify-end items-center  lg:gap-4 text-center'>
+        {/* <NavLink to="NewDocument" className={({isActive}) => `text-red-500`}>
+          New
+        </NavLink>
+        <Navlink to='NewDocument' className={({isActive}) =>`text-white text-base transition-all ${isActive ? "text-red-600" : "text-white"} text-base ml-9`}>
+        New
+        </Navlink> */}
         
-
+        <a href="NewDocument" className='lg:w-1/5'> New</a>
+        <a href="" className='lg:w-1/5 '>Saved</a>
+        <a href="" className='lg:w-1/5  '>Documents</a>
+        
       </div>
     </div>
     
--- src/component/NewDocument.jsx
+++ src/component/NewDocument.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function NewDocument() {
   return (
-    <div>NewDocument</div>
+    <>
+      <div  className=' w-full h-screen text-black bg-red-700'>
+        <div className='text-black relative '>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa dolor a quasi aperiam nam ducimus nisi omnis blanditiis deserunt. Ducimus temporibus, earum, nulla nihil sint eos dolor vel pariatur distinctio, recusandae est quae modi quam quibusdam aliquid repellat iusto provident officiis assumenda libero unde. Voluptas obcaecati nemo sed veritatis dolorum!
+        </div>
+      </div>
+    </>
   )
 }
 
