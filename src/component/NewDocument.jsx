d7a874177864008fe982ae3cedeac1117689e4c1
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-21 09:49:38 +0100

    the form is drafted

--- src/component/NewDocument.jsx
+++ src/component/NewDocument.jsx
@@ -5,7 +5,42 @@ function NewDocument() {
     <>
       <div  className='landscape w-full h-screen'>
         <div className='landscapes   w-full h-full flex  flex-col justify-center items-center '>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa dolor a quasi aperiam nam ducimus nisi omnis blanditiis deserunt. Ducimus temporibus, earum, nulla nihil sint eos dolor vel pariatur distinctio, recusandae est quae modi quam quibusdam aliquid repellat iusto provident officiis assumenda libero unde. Voluptas obcaecati nemo sed veritatis dolorum!
+            <form className='border-2 border-red-700'>
+              <div className='border-2 border-green-900 flex flex-col'>
+                <label htmlFor="">FirstName</label>
+                <input type="text" name="" id="" />
+              </div>
+              <div className='border-2 border-green-900 flex flex-col'>
+                <label htmlFor="">Surname</label>
+                <input type="text" name="" id="" />
+              </div>
+              <div className='border-2 border-green-900 flex flex-col'>
+                <label htmlFor="">Email</label>
+                <input type="email" name="" id="" />
+              </div>
+              <div className='border-2 border-green-900 flex flex-col'>
+                <label htmlFor="">Phone</label>
+                <input type="tel" name="" id="" />
+              </div>
+          
+              <div className='border-2 border-green-900 flex flex-col'>
+                <label htmlFor="">Date</label>
+                <input type="datetime" name="" id="" />
+              </div>
+              <div className='border-2 border-green-900 flex flex-col'>
+                <label htmlFor="">Review</label>
+                <textarea name="" id="" cols="30" rows="5"/>
+              </div>
+              <div className='border-2 border-green-900 flex flex-col'>
+                <label htmlFor="">Signature</label>
+                <input type="text" name="" id="" />
+              </div>
+              <div>
+                <input type="file" name="" id="" />
+              </div>
+             
+
+            </form>
         </div>
       </div>
     </>
