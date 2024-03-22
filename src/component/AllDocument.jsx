df662614155e607ab1cdcb6b8bf34b518ffd29f2
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-03-22 21:52:44 +0100

    signature pad put inplace

--- src/component/AllDocument.jsx
+++ src/component/AllDocument.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import Popup from 'reactjs-popup'
+import SignaturePad from "react-signature-canvas";
+
 
 function AllDocument() {
+  // console.log(SignaturePad)
   return (
     <>
    <div className=' landscape w-full h-screen'>
@@ -8,7 +12,9 @@ function AllDocument() {
         <div className='w-4/6 text-2xl lg:text-4xl text-white capitalize font-bigtext  px-2 '>
           <p><span className='text-5xl hover:text-red-800 cursor'>S</span>igns understands that a signature is your unique identity. The opportunity to help you discover what identity best works for you is our goal, click here to create your forever signature</p>
         </div>
-        <div className='w-4/6  relative  -bottom-14 left-1'>
+       
+          <Popup modal trigger={
+          <div className='w-4/6  relative  -bottom-14 left-1'>
             <div className='hexagon rotate-90  absolute shadow-xl  '> 
             </div>
             <div className='absolute -top-3 left-0'>
@@ -16,8 +22,14 @@ function AllDocument() {
             <span className='span'></span>
             </div>
             </div>
+            
+            
             <p className='absolute top-2  z-10 left-2 font-bigtext font-semibold tracking-wider text-2xl cursor-pointer hover:text-red-700 text-white'>clickhere</p>
-          </div>
+          </div>} >
+            {/* <h1 className='w-full p-4 bg-white text-xl'>hello</h1> */}
+            <SignaturePad canvasProps={{ className:"Signturepad"}} />
+            </Popup>
+         
       </div>
     </div>
 
--- src/component/SavedDocument.jsx
+++ src/component/SavedDocument.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Popup from 'reactjs-popup'
+import SignaturePad from "react-signature-canvas"
 
 function SavedDocument() {
   return (
-    <div>SavedDocument</div>
+    <div className='border-2 border-green-300 w-full h-screen bg-green-900 flex justify-center items-center'>
+      <div className='border-2 border-yellow-600 w-3/5 h-3/5  flex justify-center items-start'>
+
+        <Popup modal trigger={
+
+          <button type="button" className='border-2 border-black bg-white w-1/5 uppercase  font-medium  '>click me </button>
+        }>
+          <h1 className='bg-white'>hello</h1>
+          <SignaturePad />
+        </Popup>
+      </div>
+    </div>
   )
 }
 
